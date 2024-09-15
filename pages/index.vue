469e1d51350fd966c76608b420d4b2b31285ef72
mahdichangizi <mchangizi10@gmail.com>
2024-09-15 11:50:50 +0330

    re

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@ const token = useRoute().query.token;
 const total_token = ref(0);
 const username = ref(''); // می‌تونی این رو از سمت سرور بگیری
 
+
 onMounted(async () => {
   try {
     const response = await axios.get('http://localhost:8000/api/tokens', {
